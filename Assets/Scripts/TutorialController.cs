1b6fda9040cf990b703f9d3a3795e3655520a4ed
Felipe Divensi <divensifelipe@gmail.com>
2019-07-10 21:12:59 -0300

    commit tutorial

---
+++ Assets/Scripts/TutorialController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    public int paginaAtual = 0;
+
+    public List<GameObject> paginas;
+
+    public void ProximaPagina()
+    {
+        if (paginaAtual == paginas.Count - 1)
+            Debug.Log("Ultima Pagina");
+        else
+            paginaAtual++;
+
+        for (int i = 0; i < paginas.Count; i++) {
+            if (i == paginaAtual) 
+                paginas[paginaAtual].SetActive(true);
+            else
+                paginas[paginaAtual].SetActive(false);
+        }
+    }
+
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0;
+    }
+}
